7c704eb0a198e798a44f1d2709013d232004dd9c
fapb92 <69609369+fapb92@users.noreply.github.com>
2022-07-22 16:50:20 -0500

    se agrega una validación

--- middlewares/validarRegistro.js
+++ middlewares/validarRegistro.js
@@ -2,6 +2,8 @@ import { body } from "express-validator";
 import { validarErroresExpVal } from './validarErroresExpVal.js'
 
 export const validarRegistro = [
+    body("nombres", "Formato nombre no valido").trim().isString(),
+    body("nombres", "Nombre muy corto").isLength({ min: 1 }),
     body("email", "Formato de correo no valido").trim().isEmail().normalizeEmail(),
     body('password', "Mínimo 6 caracteres").isLength({ min: 6 }),
     body('password', "Password debe ser tipo string").isString(),
